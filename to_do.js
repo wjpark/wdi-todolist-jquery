ef101a76a9f9000bd3bce88c8e64e2afe6a02814
Woo Jin Park <woojinp@gmail.com>
2013-04-29 19:52:27 -0700

    implemented adding todo, marking as done, and css change when done

--- to_do.js
+++ to_do.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
-  $('button').click(function() {
-    var newTodo = $('input[id=new_todo]').val();
-    $('#todos_list ol').append('<li>' + newTodo);
-    $('#input').val('');
-  });
+  $("button").click(function() {
+    var value = $("#inputfield").val();
+    var newTodo = $("<li><input type='checkbox'>" + value + "</input><button id='delete'>delete</button></li>");
+    
+    newTodo.appendTo($("#incomplete ol"));
+    $("#inputfield").val('');
+
+		$("input:checkbox").click(function() {
+			var doneTodo = $(this);
+			if(doneTodo.is (":checked")) {
+				doneTodo.parent().appendTo("#completed ol");
+				doneTodo.parent().css("color", "red");
+				doneTodo.parent().css("text-decoration", "line-through");
+			} else {
+				doneTodo.parent().appendTo("#incomplete ol");
+				doneTodo.parent().css("color", "black");
+				doneTodo.parent().css("text-decoration", "none");
+			}
+		});
+
+		$("#delete").click(function() {
+			remove();
+		});
+	});
 });
-
-
